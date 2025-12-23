3366668935c1213e2c6f5ac7c398a1ac02364ab7
Bhanu Vinay <bhanuvinay77@gmail.com>
2025-12-23 19:18:18 +0530

    Device Events feature completed

---
+++ Geni-View Cloud/Controllers/DeviceEventsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeniView.Cloud.Controllers
+{
+    [Authorize]
+    public class DeviceEventsController : Controller
+    {
+        // GET: DeviceEvents
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
